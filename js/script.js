a31cf68c45ff5201606955c01fc74c40f8688c64
Yasir Rasool <mr.yasirrasool@gmail.com>
2025-05-04 06:00:25 +0530

    feat: add Google Analytics tracking script to script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// JavaScript code for the website
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E8RSVHS80Z"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E8RSVHS80Z');
+
